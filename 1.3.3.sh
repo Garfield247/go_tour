c57a3ae12daa0591c5d6d04523c6be787a80e98b
CatMan <garfield_lv@163.com>
2021-07-14 14:44:51 +0800

    feat(1.3.3): sub command

---
+++ 1.3.3.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+go run main.go go -name catman
+go run main.go python -n catman
--- main.go
+++ main.go
@@ -5,10 +5,25 @@ import (
 	"log"
 )
 
+var name string
+
 func main() {
-	var name string
-	flag.StringVar(&name, "name", "GO语言编程之旅", "帮助信息")
-	flag.StringVar(&name, "n", "GO语言编程之旅", "帮助信息")
 	flag.Parse()
+
+	args := flag.Args()
+	if len(args) <= 0 {
+		return
+	}
+	log.Printf("args: %v", args)
+	switch args[0] {
+	case "go":
+		goCmd := flag.NewFlagSet("go", flag.ExitOnError)
+		goCmd.StringVar(&name, "name", "GO 语言", "帮助信息")
+		_ = goCmd.Parse(args[1:])
+	case "python":
+		pyCmd := flag.NewFlagSet("python", flag.ExitOnError)
+		pyCmd.StringVar(&name, "n", "python 语言", "帮助信息")
+		_ = pyCmd.Parse(args[1:])
+	}
 	log.Printf("name: %s", name)
 }
